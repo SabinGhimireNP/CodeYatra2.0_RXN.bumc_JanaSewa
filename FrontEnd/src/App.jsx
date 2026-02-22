a7d421c5fa48cfcb1c933d3de417ce40b0030016
SabinGhimireNP <sabinghimire223@gmail.com>
2026-02-22 22:14:15 +0545

    Some UX changes

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,20 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {NavBar,ComplaintForm, TrackApplication, Footer,Login} from "./Components/index"
+import { NavBar, ComplaintForm, TrackApplication, Footer, Login, Home } from "./Components/index"
 import GovernmentNotices from "./Components/Notices/Notices";
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<>Home</>}></Route>
-        <Route path="/services" element={<></>}></Route> 
-        <Route path="/track-Application" element={<TrackApplication/>}></Route>
-        <Route path="/complaints" element={<ComplaintForm/>}></Route>
-        <Route path="/government-notice" element={<GovernmentNotices/>}></Route>
-        <Route path="/profile" element={<>profile</>}></Route>
-      </Routes>
-      <Footer/>
+      <div className="min-h-screen flex flex-col">
+        <NavBar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={< Home />}></Route>
+            <Route path="/services" element={<></>}></Route>
+            <Route path="/track-Application" element={<TrackApplication />}></Route>
+            <Route path="/complaints" element={<ComplaintForm />}></Route>
+            <Route path="/government-notice" element={<GovernmentNotices />}></Route>
+            <Route path="/profile" element={<>profile</>}></Route>
+            <Route path="/ContactUs" element={<>Contact Us</>}></Route>
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
--- FrontEnd/src/Components/index.js
+++ FrontEnd/src/Components/index.js
@@ -3,6 +3,7 @@ import ComplaintForm from "./Complaints/Complaints";
 import Footer from "./Footer/Footer";
 import Login from "./Login";
 import TrackApplication from "./Tracking/Tracking";
+import Home from "./home/Home";
 
 
-export  {NavBar, ComplaintForm, Footer, TrackApplication,Login}
+export  {NavBar, ComplaintForm, Footer, TrackApplication,Login, Home}
